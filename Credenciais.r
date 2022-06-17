ae288ef88efccab9629879049fa7ce24f8dfe8e0
paulaprist <prist@ecohealthalliance.org>
2022-06-17 09:30:16 -0400

    teste credentials

---
+++ Credenciais.r
@@ -0,0 +1,9 @@
+usethis::use_git_config(user.name="paulaprist",user.email="prist@ecohealthalliance.org")
+library(gitcreds)
+gitcreds_set()
+
+##Enter password or token: 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
+## Adding new credentials...
+## Removing credetials from cache...
+## Done.
+## this is my personal token
